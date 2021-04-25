ae33f2aed3e995b011f68419a7bcbbcf8e75dd14
Harkarman Singh <harkarman.web@gmail.com>
2021-04-25 21:54:21 +0530

    add routers - 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
 import { connect } from "react-redux";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import PropTypes from "prop-types";
 import { fetchPosts } from "../actions/posts";
 import { PostsList, Navbar } from "./";
+
+const Login = () => <div>Login</div>;
+const Signup = () => <div>Signup</div>;
+const Home = () => <div>Home</div>;
 class App extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts());
@@ -12,10 +17,26 @@ class App extends React.Component {
   render() {
     const { posts } = this.props;
     return (
-      <div>
-        <Navbar />
-        <PostsList posts={posts} />
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          {/* <PostsList posts={posts} /> */}
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+        </div>
+      </Router>
     );
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,58 +1,56 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Navbar extends Component {
-  render() {
-    return (
-      <nav className="nav">
-        <div className="left-div">
+export default function Navbar(props) {
+  return (
+    <nav className="nav">
+      <div className="left-div">
+        <img
+          src="https://raw.githubusercontent.com/Harkarman/Clover/master/assets/images/clover.png"
+          alt="logo"
+        />
+      </div>
+      <div className="search-container">
+        <img
+          src="https://image.flaticon.com/icons/svg/483/483356.svg"
+          alt="search-icon"
+        />
+        <input type="text" placeholder="Search" />
+        <div className="search-results">
+          <ul>
+            <li className="search-results-row">
+              <img
+                src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                alt="user-dp"
+              />
+              <span>John Doe</span>
+            </li>
+            <li className="search-results-row">
+              <img
+                src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                alt="user-dp"
+              />
+              <span>John Doe</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="right-nav">
+        <div className="user">
           <img
-            src="https://raw.githubusercontent.com/Harkarman/Clover/master/assets/images/clover.png"
-            alt="logo"
+            src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+            alt="user-dp"
+            id="user-dp"
           />
+          <span>John Doe</span>
         </div>
-        <div className="search-container">
-          <img
-            src="https://image.flaticon.com/icons/svg/483/483356.svg"
-            alt="search-icon"
-          />
-          <input type="text" placeholder="Search" />
-          <div className="search-results">
-            <ul>
-              <li className="search-results-row">
-                <img
-                  src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-                  alt="user-dp"
-                />
-                <span>John Doe</span>
-              </li>
-              <li className="search-results-row">
-                <img
-                  src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-                  alt="user-dp"
-                />
-                <span>John Doe</span>
-              </li>
-            </ul>
-          </div>
+        <div className="nav-links">
+          <ul>
+            <li>Login</li>
+            <li>Logout</li>
+            <li>Register</li>
+          </ul>
         </div>
-        <div className="right-nav">
-          <div className="user">
-            <img
-              src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-              alt="user-dp"
-              id="user-dp"
-            />
-            <span>John Doe</span>
-          </div>
-          <div className="nav-links">
-            <ul>
-              <li>Login</li>
-              <li>Logout</li>
-              <li>Register</li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
 }
