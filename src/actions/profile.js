e8096f86f55276a9a6c8aadccdb4c99d2de2a97e
Harkarman Singh <harkarman.web@gmail.com>
2021-05-01 23:55:11 +0530

    redirect to user profile on clicking user avatar in posts

---
+++ src/actions/profile.js
@@ -0,0 +1,36 @@
+import {
+  FETCH_USER_PROFILE,
+  USER_PROFILE_FAILED,
+  USER_PROFILE_SUCCESS,
+} from "./actionTypes";
+import { APIUrls } from "../helpers/urls";
+import { getAuthTokenFromLocalStorage } from "../helpers/utils";
+
+export function startUserProfileFetch() {
+  return { type: FETCH_USER_PROFILE };
+}
+
+export function userProfileSuccess(user) {
+  return { type: USER_PROFILE_SUCCESS, user };
+}
+
+export function userProfileFailed(error) {
+  return { type: USER_PROFILE_FAILED, error };
+}
+
+export function fetchUserProfile(userId) {
+  return (dispatch) => {
+    dispatch(startUserProfileFetch());
+    const url = APIUrls.userProfile(userId);
+    fetch(url, {
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+        Authorization: `Bearer ${getAuthTokenFromLocalStorage()}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch(userProfileSuccess(data.data.user));
+      });
+  };
+}
--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,15 @@ import {
 } from "react-router-dom";
 import PropTypes from "prop-types";
 import { fetchPosts } from "../actions/posts";
-import { Home, Navbar, Page404, Login, Signup, Settings } from "./";
+import {
+  Home,
+  Navbar,
+  Page404,
+  Login,
+  Signup,
+  Settings,
+  UserProfile,
+} from "./";
 import jwtDecode from "jwt-decode";
 import { authenticateUser } from "../actions/auth";
 import { getAuthTokenFromLocalStorage } from "../helpers/utils";
@@ -67,6 +75,11 @@ class App extends React.Component {
             <PrivateRoute
               path="/settings"
               component={Settings}
+              isLoggedin={auth.isLoggedin}
+            />
+            <PrivateRoute
+              path="/user/:userId"
+              component={UserProfile}
               isLoggedin={auth.isLoggedin}
             />
             <Route component={Page404} />
